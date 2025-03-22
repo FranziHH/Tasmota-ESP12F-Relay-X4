c55a33e55ce7ae46796e759c8989763328e8a976
FranziHH <astrid.gruenklee@yahoo.de>
2025-03-22 11:13:24 +0100

    update Template Name different for each image

--- tasmota/user_config_ESP12F_Relay_X4.h
+++ tasmota/user_config_ESP12F_Relay_X4.h
@@ -23,6 +23,8 @@
 #undef USER_TEMPLATE
 #endif
 
+#define USER_TEMPLATE_DISPLAY
+#define USER_TEMPLATE_SENSOR
 #define DISPLAY_MODE "DisplayMode 1; "
 #define CODE_IMAGE_STR_APPEND " [FW]"
 #ifdef FIRMWARE_SENSORS
@@ -44,30 +46,39 @@
             #undef CODE_IMAGE_STR_APPEND
         #endif
         #define CODE_IMAGE_STR_APPEND " BMPAHT"
+
+        #ifdef USER_TEMPLATE_SENSOR
+            #undef USER_TEMPLATE_SENSOR
+        #endif
+        #define USER_TEMPLATE_SENSOR " BMP280/AHT20"
     #else
         #ifdef CODE_IMAGE_STR_APPEND
             #undef CODE_IMAGE_STR_APPEND
         #endif
         #define CODE_IMAGE_STR_APPEND " Sensor"
+
+        #ifdef USER_TEMPLATE_SENSOR
+            #undef USER_TEMPLATE_SENSOR
+        #endif
+        #define USER_TEMPLATE_SENSOR " Sensor"
     #endif
+    
 #endif
 
 #ifdef CFG_DISPLAY
     //Display prepared
-    #if MY_LANGUAGE == de_DE
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_Display\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relais1|WebButton2 Relais2|WebButton3 Relais3|WebButton4 Relais4|WebButton5 Display\"}"
-    #else
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_Display\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relay1|WebButton2 Relay2|WebButton3 Relay3|WebButton4 Relay4|WebButton5 Display\"}"
+    #ifdef USER_TEMPLATE_DISPLAY
+        #undef USER_TEMPLATE_DISPLAY
     #endif
+    #define USER_TEMPLATE_DISPLAY " Display"
 
     #define CODE_IMAGE_STR "ESP12F-Relay-X4 Display" CODE_IMAGE_STR_APPEND
 #elif CFG_SH1106
     //SH1106 1,3'' OLED
-    #if MY_LANGUAGE == de_DE
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_SH1106\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relais1|WebButton2 Relais2|WebButton3 Relais3|WebButton4 Relais4|WebButton5 Display\"}"
-    #else
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_SH1106\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relay1|WebButton2 Relay2|WebButton3 Relay3|WebButton4 Relay4|WebButton5 Display\"}"
+    #ifdef USER_TEMPLATE_DISPLAY
+        #undef USER_TEMPLATE_DISPLAY
     #endif
+    #define USER_TEMPLATE_DISPLAY " SH1106"
 
     #define USER_RULE3 ":H,SH1106,128,64,1,I2C,3c,*,*,* :S,0,2,1,0,30,20 :I AE D5,80 A8,3f D3,00 40 8D,14 20,00 A1 C8 DA,12 81,CF D9F1 DB,40 A4 A6 AF :o,AE :O,AF :A,00,10,40,00,02 :i,A6,A7"
     #ifdef USER_BACKLOG_SENSOR
@@ -78,11 +89,10 @@
     #define CODE_IMAGE_STR "ESP12F-Relay-X4 SSH1106" CODE_IMAGE_STR_APPEND
 #elif CFG_SSD1306
     //SSD1306 0,96'' OLED
-    #if MY_LANGUAGE == de_DE
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_SSD1306\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relais1|WebButton2 Relais2|WebButton3 Relais3|WebButton4 Relais4|WebButton5 Display\"}"
-    #else
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4_SSD1306\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relay1|WebButton2 Relay2|WebButton3 Relay3|WebButton4 Relay4|WebButton5 Display\"}"
+    #ifdef USER_TEMPLATE_DISPLAY
+        #undef USER_TEMPLATE_DISPLAY
     #endif
+    #define USER_TEMPLATE_DISPLAY " SSD1306"
 
     #define USER_RULE3 ":H,SSD1306,128,64,1,I2C,3c,*,*,* :S,0,2,1,0,30,20 :I AE D5,80 A8,3F D3,00 40 8D,14 20,00 A1 C8 DA,12 81,9F D9,F1 DB,40 A4 A6 AF :o,AE :O,AF :A,00,10,40,00,00 :i,A6,A7"
     #ifdef USER_BACKLOG_SENSOR
@@ -92,11 +102,11 @@
     #endif
     #define CODE_IMAGE_STR "ESP12F-Relay-X4 SSD1306" CODE_IMAGE_STR_APPEND
 #else
-    #if MY_LANGUAGE == de_DE
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4\",\"GPIO\":[1,1,1,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relais1|WebButton2 Relais2|WebButton3 Relais3|WebButton4 Relais4\"}"
-    #else
-        #define USER_TEMPLATE "{\"NAME\":\"ESP12F_Relay_X4\",\"GPIO\":[1,1,1,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relay1|WebButton2 Relay2|WebButton3 Relay3|WebButton4 Relay4\"}"
-    #endif
-    
     #define CODE_IMAGE_STR "ESP12F-Relay-X4" CODE_IMAGE_STR_APPEND
+#endif
+
+#if MY_LANGUAGE == de_DE
+    #define USER_TEMPLATE "{\"NAME\":\"ESP12F-Relay-X4" USER_TEMPLATE_DISPLAY USER_TEMPLATE_SENSOR"\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relais1|WebButton2 Relais2|WebButton3 Relais3|WebButton4 Relais4|WebButton5 Display\"}"
+#else
+    #define USER_TEMPLATE "{\"NAME\":\"ESP12F-Relay-X4" USER_TEMPLATE_DISPLAY USER_TEMPLATE_SENSOR"\",\"GPIO\":[640,5730,608,1,1,320,1,1,225,224,226,227,1,1],\"FLAG\":0,\"BASE\":18,\"CMND\":\"WebButton1 Relay1|WebButton2 Relay2|WebButton3 Relay3|WebButton4 Relay4|WebButton5 Display\"}"
 #endif
